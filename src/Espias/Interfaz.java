21b09e8c96f294012d231d938723f47d62922515
Lucas Prieto <lucasprieto30@gmail.com>
2022-05-07 18:48:32 -0300

    add mensaje de posible encuentro agregado y visuales

--- src/Espias/Interfaz.java
+++ src/Espias/Interfaz.java
@@ -43,9 +43,9 @@ public class Interfaz {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(500, 250, 633, 329);
+		frame.setBounds(500, 250, 633, 380);
 		frame.setTitle("Temible operario del recontraespionaje");
-		frame.getContentPane().setBackground(Color.GRAY);;
+		frame.getContentPane().setBackground(new Color(72, 209, 204));;
 		frame.getContentPane().setLayout(null);
 
 		comboEspias1 = new JComboBox();
@@ -67,14 +67,15 @@ public class Interfaz {
 		frame.getContentPane().add(comboEspias2);
 
 		JLabel lblAgregarPosiblesEncuentros = new JLabel("Agregar posibles encuentros");
-		lblAgregarPosiblesEncuentros.setBounds(215, 10, 324, 31);
+		lblAgregarPosiblesEncuentros.setFont(new Font("Verdana", Font.PLAIN, 20));
+		lblAgregarPosiblesEncuentros.setBounds(154, 10, 324, 31);
 		frame.getContentPane().add(lblAgregarPosiblesEncuentros);
 
 		JLabel lblEspia = new JLabel("Espia");
 		lblEspia.setBounds(86, 72, 46, 31);
 		frame.getContentPane().add(lblEspia);
 
-		JLabel lblProbabilidad = new JLabel("Probabilidad de intercepci�n");
+		JLabel lblProbabilidad = new JLabel("Probabilidad de intercepción");
 		lblProbabilidad.setBounds(225, 55, 210, 65);
 		frame.getContentPane().add(lblProbabilidad);
 
@@ -82,33 +83,38 @@ public class Interfaz {
 		lblEspia_1.setBounds(445, 67, 46, 40);
 		frame.getContentPane().add(lblEspia_1);
 		
-		JLabel lblMensajeError = new JLabel("");
-		lblMensajeError.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMensajeError.setForeground(new Color(128, 0, 0));
-		lblMensajeError.setBounds(51, 161, 488, 31);
-		frame.getContentPane().add(lblMensajeError);
-		
+		JLabel lblMensajeAlAgregar = new JLabel("");
+		lblMensajeAlAgregar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMensajeAlAgregar.setForeground(new Color(128, 0, 0));
+		lblMensajeAlAgregar.setBounds(51, 161, 488, 31);
+		frame.getContentPane().add(lblMensajeAlAgregar);
+
 		JTextArea lblResultados = new JTextArea("");
 		lblResultados.setEditable(false);
 		lblResultados.setOpaque(false);
-		lblResultados.setBounds(51, 36, 488, 155);
+		lblResultados.setBounds(61, 10, 488, 237);
+		lblResultados.setForeground(new Color(0, 0, 0));
+		lblResultados.setFont(new Font("Tahoma", Font.BOLD, 12));
 		frame.getContentPane().add(lblResultados);
 
 		JButton btnAgregar = new JButton("Agregar");
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				lblMensajeAlAgregar.setForeground(new Color(128, 0, 0));
+
 				int espiaSeleccionado1 = comboEspias1.getSelectedIndex();
 				int espiaSeleccionado2 = comboEspias2.getSelectedIndex();
 
 				if(espiaSeleccionado1 == espiaSeleccionado2)
-					lblMensajeError.setText("No puede haber un encuentro entre un mismo esp�a!");
+					lblMensajeAlAgregar.setText("No puede haber un encuentro entre un mismo espía!");
 				
 				if (espias.getRedEspias().existeArista(espiaSeleccionado1, espiaSeleccionado2))
-					lblMensajeError.setText("Ese posible encuentro ya fue agregado!");
+					lblMensajeAlAgregar.setText("Ese posible encuentro ya fue agregado!");
 				
 				if(espiaSeleccionado1 != espiaSeleccionado2 && !espias.getRedEspias().existeArista(espiaSeleccionado1, espiaSeleccionado2)) {
 					espias.agregarPosibleEncuentro(comboEspias1.getSelectedIndex(), comboEspias2.getSelectedIndex(), (double) probabilidades.getSelectedItem());
-					lblMensajeError.setText("");
+					lblMensajeAlAgregar.setForeground(new Color(0,128 , 0));
+					lblMensajeAlAgregar.setText("Posible encuentro agregado: "+ comboEspias1.getSelectedItem() + " y " + comboEspias2.getSelectedItem() + ". Probabilidad: " + probabilidades.getSelectedItem());
 				}
 			}
 		});
@@ -121,6 +127,7 @@ public class Interfaz {
 				comboEspias1.setVisible(false);
 				probabilidades.setVisible(false);
 				comboEspias2.setVisible(false);
+				lblMensajeAlAgregar.setVisible(false);
 				lblAgregarPosiblesEncuentros.setVisible(false);
 				lblEspia.setVisible(false);
 				lblProbabilidad.setVisible(false);
@@ -131,11 +138,11 @@ public class Interfaz {
 				btnResultado.setEnabled(false);
 			}
 		});
-		btnResultado.setBounds(225, 258, 145, 21);
+		btnResultado.setBounds(227, 288, 145, 21);
 		frame.getContentPane().add(btnResultado);
 
 		JSeparator separator = new JSeparator();
-		separator.setBounds(-48, 248, 702, 12);
+		separator.setBounds(-47, 257, 702, 12);
 		frame.getContentPane().add(separator);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -149,6 +156,9 @@ public class Interfaz {
 		HashMap<Tupla<Integer, Integer>, Double> resultados = espias.redEspiasConMenorRiesgo();
 		System.out.println(resultados);
 		
+		if(resultados.size() == 0)
+			return "No es pobible comunicar a todos los espias con los valores ingresados";
+
 		for (Entry<Tupla<Integer, Integer>, Double> r: resultados.entrySet()) {
 			String espia1 = comboEspias1.getItemAt(r.getKey().getX()).toString();
 			String espia2 = comboEspias1.getItemAt(r.getKey().getY()).toString();
